a1c509e4d5f0b17adcea954eebed448363ca8f66
Melvin Mendoza <mcamendoza1@gmail.com>
2024-02-01 11:07:07 +0800

    added session

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import datetime
 import json
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from intuitlib.client import AuthClient
 from intuitlib.enums import Scopes
 
@@ -11,6 +11,7 @@ from quickbooks.objects.invoice import Invoice
 
 
 app = Flask(__name__)
+app.secret_key = 'your secret key'
 
 # Load the stored environment variables
 load_dotenv()
@@ -19,18 +20,39 @@ auth_client = AuthClient(
     client_id     = os.getenv("CLIENT_ID"),
     client_secret = os.getenv("CLIENT_SECRET"),
     environment   = os.getenv("ENVIRONMENT"),
-    redirect_uri  = os.getenv("REDIRECT_URI"),
+    redirect_uri  = os.getenv("REDIRECT_URI"),    
 )
 
+realm_id = os.getenv("REALM_ID")
+
 @app.route('/')
-def index():    
+def index():        
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
     url = auth_client.get_authorization_url([Scopes.ACCOUNTING])
     return redirect(url)
+
+@app.route('/test')
+def test():
+    refresh_token = session.get('refresh_token')
+    access_token = session.get('access_token')
+    
+    print(refresh_token)
+    print(access_token)
+    print(realm_id)
+    return auth_client.access_token
 
 @app.route('/callback')
 def callback():
     auth_code = request.args.get('code')
-    auth_client.get_bearer_token(auth_code, realm_id='9130357842152386')
+    auth_client.get_bearer_token(auth_code, realm_id=realm_id)
+
+    # Store the access token, refresh token, and token expiration time in session
+    session['access_token'] = auth_client.access_token  
+    session['refresh_token'] = auth_client.refresh_token
+    # session['token_expires_at'] = auth_client.token_expires_at
     # print(auth_client.access_token)
 
     client = QuickBooks(
@@ -39,10 +61,6 @@ def callback():
         company_id='9130357842152386'
     )
 
-    # invoices = Invoice.all(qb=client, max_results=100)
-    # invoices = Invoice.filter(CustomerRef='16', order_by='TxnDate DESC', qb=client)
-    # date_after = datetime(2022, 1, 1)
-    # invoices = Invoice.filter(MetaData_CreateTime__gt=date_after, qb=client)
     invoice_number = '1010'
     invoices = Invoice.filter(DocNumber=invoice_number, qb=client)
 
@@ -51,25 +69,7 @@ def callback():
         invoice_json = json.dumps(invoice_dict)
         print(invoice_json)
 
-    # for invoice in invoices:
-    #     print(dir(invoice))
-    #     print(invoice.BillAddr.Line1)
-    #     print(invoice.BillAddr.Line2)
-    #     print(invoice.BillAddr.City)
-    #     print(invoice.BillAddr.Country)
-    #     print(invoice.BillAddr.PostalCode)
-    #     print(invoice.TxnTaxDetail)
-    #     # print(invoice.Line)
-        
-    #     for line in invoice.Line:
-    #         print('====')
-    #         print(dir(line))
-    #         print(line.Id)
-    #         print(line.Description)
-    #         print(line.Amount)
-    #         print(line.SalesItemLineDetail())
-
-    return render_template('index.html')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
